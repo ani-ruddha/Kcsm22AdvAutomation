304185f682ee2df8b0135f7e41d2d1834585ab4e
ani-ruddha <aniruddhadas919@gmail.com>
2023-11-17 12:11:56 +0530

    1st push by te1

--- src/test/java/sample/Demo.java
+++ src/test/java/sample/Demo.java
@@ -5,5 +5,7 @@ public class Demo {
 	public static void main(String[] args) {
 		
 		System.out.println("push demo class to gitHub");
+		
+		
 	}
 }
--- src/test/java/sample/Test.java
+++ src/test/java/sample/Test.java
@@ -9,5 +9,6 @@ public class Test {
 		System.out.println("push test 2nd time ");
 		
 		
+		System.out.println("welcome");
 	}
 }
